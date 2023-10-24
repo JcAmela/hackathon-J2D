6115258f69505c8fb4a70418cacd47967718677a
JcAmela <juancarlosamela@gmail.com>
2023-10-24 09:51:19 +0200

    Improve responsiveness of the title and filter component, and add 'not found' error message

--- src/app/core/character/filter/filter.component.ts
+++ src/app/core/character/filter/filter.component.ts
@@ -13,6 +13,7 @@ export class FilterComponent implements AfterViewInit {
   speciesFilter: string = '';
   typeFilter: string = '';
   genderFilter: string = '';
+  errorMessage: string = '';
 
   @Input() characters: Character[] = [];
   @Output() filtered = new EventEmitter<{ characters: Character[], isEmpty: boolean, userInteracted: boolean }>();
@@ -37,16 +38,21 @@ export class FilterComponent implements AfterViewInit {
     const userInteracted = !!this.searchTerm || !!this.statusFilter || !!this.speciesFilter || !!this.typeFilter || !!this.genderFilter;
 
     this.CharactersService.getCharactersByFilter(filters).subscribe(response => {
+      if (response.results.length === 0) {
+        this.errorMessage = 'No matches found.';
+      } else {
+        this.errorMessage = '';
+      }
+
       this.filtered.emit({
         characters: response.results,
         isEmpty: response.results.length === 0,
         userInteracted: userInteracted
       });
     }, error => {
-      console.error('Error al obtener personajes:', error);
+      console.error('Error fetching characters:', error);
+      this.errorMessage = 'There was an error fetching the characters. Please try again.';
     });
-  }
-
-
+}
 
 }
